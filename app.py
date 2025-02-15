7e21ef6da2e3e83d1fe5e0a2730ea4cfa0d53eae
Sujal Singh Rawat <129302673+sujalrawat884@users.noreply.github.com>
2025-02-15 23:11:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,36 @@ import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+st.markdown("""
+    <style>
+      [data-testid="stAppViewContainer"] {
+        background-image: url("https://images.unsplash.com/photo-1628348070889-cb656235b4eb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        background-size: cover;
+      }
+      [data-testid="stHeader"] {
+        background-color: rgba(0, 0, 0, 0);  
+      }
+      [data-testid="stToolbar"] {
+        right: 2rem;
+      }
+      .st-emotion-cache-czk5ss.e16jpq800 {
+        visibility: hidden;
+      }
+      .st-emotion-cache-mnu3yk.ef3psqc5 {
+        visibility: hidden;
+      }
+      .st-emotion-cache-15ecox0.ezrtsby0 {
+        visibility: hidden;
+      }
+      .st-emotion-cache-fm8pe0.e1nzilvr4 {
+        color: black;
+      }
+      .st-emotion-cache-fm8pe0.e1nzilvr4{
+        color: white;
+      }
+    </style>
+    """, unsafe_allow_html=True)
+
 # Load trained model
 with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
